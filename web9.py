6045810d200128c7a57ab05f664dfb266a05fc24
SkillfulGuru <45273149+SkillfulGuru@users.noreply.github.com>
2018-11-25 17:59:09 +1300

    Add files via upload

---
+++ web9.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import requests
+import datetime
+import subprocess
+
+myurl = 'https://www.nzx.com/markets/NZSX'
+
+myweb_data = requests.get(myurl)
+myweb_data.encoding = 'utf-8'
+mysoup = BeautifulSoup(myweb_data.text, 'lxml')
+
+stuff = mysoup.find('table', {'id': 'instruments-table'})
+
+# extract instrument names
+stuff2 = stuff.findAll('a')
+for i in range(0,len(stuff2)):
+	stuff2[i] = stuff2[i].text.strip()
+
+_length = int(len(stuff2)/2)
+for i in range(0,_length):
+	del stuff2[i]
+
+# extract prices
+prices = mysoup.findAll('td', {'data-title':'Price'})
+for i in range(0,len(stuff2)):
+	prices[i] = prices[i].text.strip()
+
+# extract instrument name abbreviations	
+abv = stuff.findAll('strong')
+for i in range(0,len(stuff2)):
+	abv[i] = abv[i].text.strip()
+
+# create empty list
+_list = [None] * len(stuff2)
+
+# combine each instrument name, abbreviation and price into one list
+for i in range(0,len(stuff2)):
+	stringy = "{0}           {1}   {2}".format("%-5s" % abv[i], "%-60s" % stuff2[i], "%7s" % prices[i])
+	_list[i] = stringy
+
+# sort the list alphabetically
+_list.sort()
+
+# display list
+for i in sorted(_list):
+	print(i)
+	print("")
+
+file = open("output.txt", "w+", encoding="utf-8")
+file.write("{0}    {1}                                                      {2}".format("ABBREVIATION" , "INSTRUMENT", "PRICE\n"))
+file.write("-----------------------------------------------------------------------------------------\n")
+			
+for item in _list:
+	file.write("{}\n".format(item))
+file.close()
+
+# open file in notepad
+txtfile = "output.txt"
+ntepad = r'C:\Windows\System32\notepad.exe'
+subprocess.Popen("%s %s" % (ntepad, txtfile))
